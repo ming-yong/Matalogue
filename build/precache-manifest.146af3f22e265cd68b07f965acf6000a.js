d269381da36574f843dac3c38a1a8e4061c0e499
mingyong.dev <mingyong.dev@gmail.com>
2020-08-22 17:13:05 -0400

    I need the build?

---
+++ build/precache-manifest.146af3f22e265cd68b07f965acf6000a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c27c11bd4204067a2d6a73a61f3ff659",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fce323290e1e50f25612",
+    "url": "/static/css/main.1c2a43e2.chunk.css"
+  },
+  {
+    "revision": "3182f9733e7abb51269b",
+    "url": "/static/js/2.c143373b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c143373b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fce323290e1e50f25612",
+    "url": "/static/js/main.3cfaa715.chunk.js"
+  },
+  {
+    "revision": "752dfbcfe4907915b9da",
+    "url": "/static/js/runtime-main.618cba32.js"
+  }
+]);
