ad19558d8da2669ce744ad87e528dc738a4d57f2
mingyong.dev <mingyong.dev@gmail.com>
2020-08-22 17:32:55 -0400

    why

--- src/index.js
+++ src/index.js
@@ -1,17 +1,108 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Matalogue extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			step: 1,
+			userName: "",
+			articleAmount: 100,
+			dateStart: "",
+			dateEnd: "",
+		};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+		this.submitInfo = this.submitInfo.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.resetAll = this.resetAll.bind(this);
+	}
+
+	handleChange(event) {
+		const target = event.target;
+		this.setState({
+			[target.name]: target.value,
+		});
+	}
+
+	submitInfo(event) {
+		alert(this.state.articleAmount);
+		if (this.state.userName !== "") {
+			this.setState({ step: 2 });
+		}
+		event.preventDefault();
+	}
+
+	resetAll(event) {
+		this.setState({ step: 1 });
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header>
+					<h1>Matalogue</h1>
+				</header>
+				<Form step={this.state.step} userName={this.state.userName} dateStart={this.state.dateStart} dateEnd={this.state.dateEnd} articleAmount={this.state.articleAmount} submitInfo={this.submitInfo} handleChange={this.handleChange} />
+				<footer className="footer">
+					<button>
+						<a href="#" className="credit">
+							🥕豆腐制作
+						</a>
+					</button>
+					<button onClick={this.resetAll}>🧄重来</button>
+				</footer>
+			</div>
+		);
+	}
+}
+
+function Form(props) {
+	switch (props.step) {
+		case 2:
+			return <Result />;
+		default:
+			return <GrabFrom userName={props.userName} articleAmount={props.articleAmount} dateStart={props.dateStart} dateEnd={props.dateEnd} submitInfo={props.submitInfo} handleChange={props.handleChange} />;
+	}
+}
+
+function GrabFrom(props) {
+	return (
+		<form onSubmit={props.submitInfo}>
+			<div className="row">
+				<label>
+					用户名
+					<input type="text" name="userName" onChange={props.handleChange} value={props.userName} />
+				</label>
+			</div>
+
+			<div className="row">
+				<label>
+					文章数量
+					<input type="number" name="articleAmount" onChange={props.handleChange} value={props.articleAmount} />
+				</label>
+			</div>
+
+			<div className="row">
+				<label>
+					从
+					<input type="date" name="dateStart" onChange={props.handleChange} value={props.dateStart} />
+				</label>
+				<label>
+					到
+					<input type="date" name="dateEnd" onChange={props.handleChange} value={props.dateEnd} />
+				</label>
+			</div>
+
+			<div className="row">
+				<input type="submit" value="抓！" className="btn" />
+			</div>
+		</form>
+	);
+}
+
+function Result(props) {
+	return <form onSubmit={props.handleSubmit}></form>;
+}
+
+ReactDOM.render(<Matalogue />, document.getElementById("root"));
